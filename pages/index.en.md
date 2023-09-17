ce769bde25d9ebc5a1986a3485b126025c038a0d
zmjack <jack@zmland.com>
2023-09-17 11:44:19 +0800

    nextra

---
+++ pages/index.en.md
@@ -0,0 +1,3 @@
+# Welcome to LinqSharp
+ 
+Hello, world!
---
+++ theme.config.jsx
@@ -0,0 +1,15 @@
+export default {
+  logo: <span>LinqSharp Documentation</span>,
+  project: {
+    link: 'https://github.com/zmjack/linqsharp'
+  },
+  // ... other theme options
+  docsRepositoryBase: 'https://github.com/zmjack/linqsharp-doc',
+  search: {
+    placeholder: 'Search...'
+  },
+  i18n: [
+    { locale: 'en', text: 'English' },
+    { locale: 'zh', text: '中文' }
+  ],
+}
