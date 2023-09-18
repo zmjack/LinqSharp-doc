7fb8b8ebb49188bdd512716d5685013957ff1ace
zmjack <jack@zmland.com>
2023-09-18 19:32:45 +0800

    update

--- theme.config.jsx
+++ theme.config.jsx
@@ -8,7 +8,33 @@ export default {
   search: {
     placeholder: 'Search...'
   },
+  toc: {
+    title: 'On This Page'
+  },
+  editLink: {
+    text: 'Edit this page',
+    component: function (props) {
+      return (
+        <a
+          className={props.className}
+          href={`https://github.com/zmjack/linqsharp-doc/blob/main/${props.filePath}`}
+        >
+          {props.children}
+        </a>
+      );
+    }
+  },
   feedback: {
-    useLink: () => 'https://github.com/zmjack/linqsharp/issues/new?labels=feedback'
+    useLink: () => 'https://github.com/zmjack/linqsharp/issues/new?labels=feedback',
+    content: 'Question? Give us feedback →'
+  },
+  themeSwitch: {
+    useOptions() {
+      return {
+        light: 'Light',
+        dark: 'Dark',
+        system: 'System'
+      }
+    }
   }
 }
