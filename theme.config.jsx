6bc4df8990a157de674b49e57f8d9739a3971dbd
zmjack <jack@zmland.com>
2023-11-14 02:12:38 +0800

    update

--- theme.config.jsx
+++ theme.config.jsx
@@ -17,7 +17,7 @@ export default {
     key: 'zh-site',
     text: (
       <a href="https://zh.linqsharp.net" target="_blank">
-        🎉 您的语言为 中文，推荐访问 LinqSharp 中文站 →
+        🎉 您的浏览器语言为 中文，推荐访问 LinqSharp 中文站 →
       </a>
     )
   },
